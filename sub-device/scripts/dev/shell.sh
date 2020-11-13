d90bc3ff4928528d8c4870f02f933b1d04a080ad
Mickxuanyuan <303062086@qq.com>
2020-11-13 15:45:28 +0800

    添加发版方式

--- sub-device/scripts/dev/shell.sh
+++ sub-device/scripts/dev/shell.sh
@@ -2,12 +2,12 @@
 # 当前镜像版本号
 version="1.0.0";
 # 镜像名
-imageName= "sub-device__test"
-containerName= "sub-device__test"
+imageName= "sub-device__test";
+containerName= "sub-device__test";
 # jenkins服务器开放的的前端访问端口
-port="2155"
+port="2155";
 # 镜像中server暴露的端口号
-exposePort="9527"
+exposePort="9527";
 ``
 echo "--> yarn install"
 cd ../..
