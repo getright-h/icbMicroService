348b894c1dc4609babb040a8414d77947fa451c2
Mickxuanyuan <303062086@qq.com>
2020-12-26 18:32:26 +0800

    修改发版方法

--- sub-upms/scripts/dev/shell.sh
+++ sub-upms/scripts/dev/shell.sh
@@ -2,8 +2,8 @@
 # 当前镜像版本号
 version="1.0.0";
 # 镜像名
-imageName="sub-device_test";
-containerName="sub-device_test";
+imageName="test_sub_upms";
+containerName="test_sub_upms";
 # jenkins服务器开放的的前端访问端口
 port="2099";
 # 镜像中server暴露的端口号
