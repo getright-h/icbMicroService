fe4a2f6b55d1654e886db5304245981b640fc642
fanwenbo <listenecho@foxmail.com>
2021-02-19 09:48:38 +0800

    校验渲染

--- sub-electric-fence/src/framework/aop/hooks/use-authority-state.ts
+++ sub-electric-fence/src/framework/aop/hooks/use-authority-state.ts
@@ -1,6 +1,7 @@
 import { GlobalContext } from '~/solution/context/global/global.provider';
 import { IGlobalState } from '~/solution/context/global/global.interface';
 import { useLocation } from 'react-router-dom';
+import { isBoolean } from 'lodash';
 
 export function useAuthorityState() {
   const { gState }: IGlobalState = React.useContext(GlobalContext);
@@ -14,4 +15,20 @@ export function useAuthorityState() {
   return {
     authority
   };
+}
+
+/**
+ *
+ * @param authority 权限码hash表
+ * @param key 需要校验的权限码
+ */
+export function authorityRender(authority: Record<string, any>, key: string): boolean {
+  // 如果当前页面没有权限列表,或者未传递需要校验的权限码,则返回 true
+  if (!Object.keys(authority).length || typeof key != 'string' || !key) return true;
+  // 只是针对权限是否是 boolen类型 才对权限进行判断, 否则 返回 true
+  if (isBoolean(authority[key])) {
+    return authority[key];
+  } else {
+    return true;
+  }
 }
