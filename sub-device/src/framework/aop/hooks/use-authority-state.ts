94b94ab08900a33d22c9ad50d498e8e5aff2fcab
fanwenbo <listenecho@foxmail.com>
2021-02-22 10:15:57 +0800

    1. 设备子项目,父子路由关系表

--- sub-device/src/framework/aop/hooks/use-authority-state.ts
+++ sub-device/src/framework/aop/hooks/use-authority-state.ts
@@ -97,7 +97,7 @@ export function usePath() {
  */
 export function useAuthorityRender(authority: Record<string, any>, key: string): boolean {
   // 如果当前页面没有权限列表,或者未传递需要校验的权限码,则返回 true
-  if (!Object.keys(authority).length || typeof key != 'string' || !key) return true;
+  if (!authority || !Object.keys(authority).length || typeof key != 'string' || !key) return true;
 
   // 只是针对权限是否是 boolen类型 才对权限进行判断, 否则 返回 true
   if (isBoolean(authority[key])) {
--- sub-device/src/solution/shared/constant/menu.map.ts
+++ sub-device/src/solution/shared/constant/menu.map.ts
@@ -1,3 +1,57 @@
-export const MENU_MAP = {
-  '/home/device/allocation/process': ['/home/device/allocation/initDetail']
+export const MENU_MAP: any = {
+  /** 库存管理 */
+
+  // 全部设备管理
+  '/home/stock/all': [],
+  // 采购单管理
+  '/home/stock/purchase': [],
+  // 出入库管理
+  '/home/stock/in-out': [],
+
+  /** 设备调拨 */
+  // 调拨单
+  '/home/device/allocation/manage': [
+    '/home/device/allocation/initDetail' // 调拨单详情
+  ],
+
+  // 调拨流程
+  '/home/device/allocation/process': [
+    '/home/device/allocation/initDetail' // 调拨单详情
+  ],
+
+  /** 监控组 */
+  // 监控组管理
+  '/home/monitor/monitorManage': [],
+  /** 数据报表 */
+
+  // 设备路线表
+  '/home/data/deviceLine': [],
+
+  /** 基础管理 */
+  // 仓库设置
+  '/home/baseManage/warehouse': [],
+  // 设备型号设置
+
+  '/home/baseManage/deviceTypeSetting': [],
+
+  /** 审批管理 */
+  // 审批管理
+  '/home/approvalManage/approvalManage': [
+    '/home/approvalManage/approvalManageDetail', // 审批管理详情
+    '/home/approvalManage/approvalEditTemplateFormModal', // 审批管理编辑
+    '/home/approvalManage/approvalTemplateFormModal' // 新增审批
+  ],
+  // 审批模板
+  '/home/approvalManage/approvalTemplate': [
+    '/home/approvalManage/addTemplate' // 创建审批模板 OR 编辑审批模板
+  ],
+  /** 客户管理 */
+  // 车主管理
+  '/home/customer/owner': [],
+  // 车辆管理
+  '/home/customer/vehicle': [
+    '/home/customer/vehicleDetail' // 车辆详情
+  ],
+  // 安装凭证管理
+  '/home/voucher/manage': []
 };
