68ebb4478dbab0dee24510408d6279ce56eff67a
wangyuxin <369264448@qq.com>
2021-02-23 16:56:44 +0800

    修复gState初始myInfo.typeId

--- sub-electric-fence/src/solution/context/global/global.provider.tsx
+++ sub-electric-fence/src/solution/context/global/global.provider.tsx
@@ -7,10 +7,10 @@ const GlobalProvider = (props: any) => {
   initialState.myInfo = !(props && props.userInfo)
     ? {
         userId: '',
-        systemId: '',
-        typeId: ''
+        typeId: 'c59c75eec2d3cc075cca08d84386bcb9',
+        systemId: '938880216d89c68eb6ea08d69b143c52'
       }
-    : { ...props.userInfo, userId: props.userInfo.id };
+    : { ...props.userInfo, userId: props.userInfo.id, typeId: 'c59c75eec2d3cc075cca08d84386bcb9' };
   const [state, dispatch] = useReducer(reducer, initialState);
   return <GlobalContext.Provider value={{ gState: state, dispatch }}>{props.children}</GlobalContext.Provider>;
 };
