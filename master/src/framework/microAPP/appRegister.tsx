7b45e7be96e4c17399f19d0a6efbe8d998e1cbaa
fanwebo <listenecho@foxmail.com>
2021-03-30 09:55:13 +0800

    修复token

--- master/src/framework/microAPP/appRegister.tsx
+++ master/src/framework/microAPP/appRegister.tsx
@@ -38,7 +38,7 @@ async function registerMainApp(callback: () => any) {
 
       entry: isDev ? localURL : onLineURL,
       container: currentId,
-      activeRule: [(isDev ? '/' : '/gpssass') + `/#${path}`, (isDev ? '/' : '/gpssass') + `#${path}`],
+      activeRule: [(isDev ? '/' : '/gpssass') + `#${path}`, (isDev ? '/' : '/gpssass') + `#${path}`],
       // activeRule: `/#${path}`,
       //activeRule: () => [(isDev ? '/' : '/gpssass') + `/#${path}`, (isDev ? '/' : '/gpssass') + `#${path}`],
       props: { baseFuntion, name, routers: JSON.parse(JSON.stringify(children)), routerBase: `/#${path}`, userInfo }
